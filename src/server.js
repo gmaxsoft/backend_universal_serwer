c70d2fa494f08f4eef4599f56f9db03ddf8a108b
Grzegorz Miśków <133238725+gmaxsoft@users.noreply.github.com>
2026-01-02 21:51:04 +0100

    Konfiguracja dokera,prisma i rozbudowanie pliku gitignore

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { PrismaClient } from './generated/prisma'
+const prisma = new PrismaClient();
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+//Endpoint testowy
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
 
 //Serwer
 app.listen(PORT, () => {
